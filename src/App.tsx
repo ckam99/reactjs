f77af9fafc394c86a7a0b219d2430199f7d1c711
Ckam <ckam225@gmail.com>
2021-10-11 12:32:50 -0700

    added eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import IMAGE from './react.png'
 import logo from './logo.svg'
 
 function App() {
-    return <div>
-        <img src={logo} alt="" />
-        <img src={IMAGE} alt="" />
-        <h1>React and Typescript</h1>
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <img src={IMAGE} alt="" />
+      <h1>React and Typescript</h1>
     </div>
+  )
 }
 
 export default App
