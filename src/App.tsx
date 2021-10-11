236661949d65ef19f6f8e7ef34041dda56b6b6e3
Ckam <ckam225@gmail.com>
2021-10-11 11:32:43 -0700

    font loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './style.css'
+import IMAGE from './react.png'
 import logo from './logo.svg'
 
 function App() {
     return <div>
+        <img src={logo} alt="" />
+        <img src={IMAGE} alt="" />
         <h1>React and Typescript</h1>
     </div>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,15 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
         ]
     },
     output: {
