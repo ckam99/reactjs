113ba43b4fe6dd1bb879cc4726aedf44533c3527
Ckam <ckam225@gmail.com>
2022-07-09 14:12:43 +0300

    configuration eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function App() {
-  return <React.Fragment>
-    <h1>Hello</h1>
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <h1 className="sfsd">Hello</h1>
+      <h1 className="sfsd">Hello</h1>
+      <h1 className="sfsd">Hello</h1>
+      <h1 className="sfsd">Hello</h1>
+      <h1 className="sfsd">Hello</h1>
+      <h1 className="sfsd">Hello</h1>
+    </React.Fragment>
+  );
 }
 
 export default App;
